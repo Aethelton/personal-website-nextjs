6eabc1dcb720c10a691feae58769d42ba2f3761e
Eston <liyuelin2004@gmail.com>
2022-04-10 18:00:27 -0400

    Added Progress Bar and Custom Scrollbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import dynamic from "next/dynamic";
+
+const ProgressBar = dynamic(() => import("/components/ProgressBar"), { ssr: false });
 
 const MyApp = ({Component, pageProps}) => {
     return (
@@ -9,6 +12,7 @@ const MyApp = ({Component, pageProps}) => {
                 <meta name="viewport" content="width=device-width, minimum-scale=1" />
             </Head>
             <Component {...pageProps} />
+            <ProgressBar />
         </>
     );
 };
